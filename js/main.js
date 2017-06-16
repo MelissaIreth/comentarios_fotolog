5c7139f8143ae43b6dbdb824f43c4611cfe1899f
Melissa Pacheco <melissa.pacheco.martinez@gmail.com>
2017-06-16 18:26:56 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,28 +1,55 @@
 /*aquí va tu código*/
 function init(){
 	var button = document.getElementById("boton");
-	button.addEventListener("click", guardar);
+	button.addEventListener("click", datos);
 	guardar();	
 }
-function guardar(){
+
+function datos(){
 	var clave = document.getElementById("key").value;
 	var valor = document.getElementById("value").value;
 
 	localStorage.setItem(clave, valor);
+	guardar();
 
+	document.getElementById("key").value = "";
+	document.getElementById("value").value = "";
+}
+
+function guardar(){
 	var contenedor = document.getElementById("comentario-final");
 	contenedor.innerHTML = "";
+
 	for(var i = 0; i < localStorage.length; i++){
 		var el_key = localStorage.key(i);
-		var el_valor = localStorage.getItem(el_key);
+		var el_item = localStorage.getItem(el_key);
 
-		contenedor.innerHTML += "<p>" + el_key + " " + 
-		el_valor + "</p>";
+		var div = document.createElement("div");
+		div.classList.add("div-comentario");
+
+		var el_titulo = document.createElement("h4");
+		el_titulo.classList.add("eltitulo");
+		var key_new = document.createTextNode(el_key);
+		el_titulo.appendChild(key_new);
+
+		var el_texto = document.createElement("p");
+		el_texto.classList.add("el-texto");
+		var new_item = document.createTextNode(el_item);
+		el_texto.appendChild(new_item);
+
+		div.appendChild(el_titulo);
+		div.appendChild(el_texto);
+		contenedor.appendChild(div);
 	}
 
 }
-function cleanData(){
-	document.getElementById("comentario-final").innerHTML = "";
-}
 
 init();
+
+var clear = document.getElementById("boton-clean");
+clear.addEventListener("click", cleanData);
+
+function cleanData(){
+	window.localStorage.clear();
+	guardar();
+}
