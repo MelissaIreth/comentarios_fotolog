49904440bf33f4b1d2849c9ee8a619b355ee8a96
Melissa <melissa.pacheco.martinez@gmail.com>
2017-06-16 13:53:52 -0400

    inicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 /*aquí va tu código*/
+function init(){
+	var button = document.getElementById("boton");
+	button.addEventListener("click", guardar);
+	guardar();	
+}
+function guardar(){
+	var clave = document.getElementById("key").value;
+	var valor = document.getElementById("value").value;
+
+	localStorage.setItem(clave, valor);
+
+	var contenedor = document.getElementById("comentario-final");
+	contenedor.innerHTML = "";
+	for(var i = 0; i < localStorage.length; i++){
+		var el_key = localStorage.key(i);
+		var el_valor = localStorage.getItem(el_key);
+
+		contenedor.innerHTML += "<p>" + el_key + " " + 
+		el_valor + "</p>";
+	}
+
+}
+function cleanData(){
+	document.getElementById("comentario-final").innerHTML = "";
+}
+
+init();
